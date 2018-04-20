571d743913595eebc94ee426663a4e676fdfd963
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-20 16:50:10 -0500

    definir variables en typescript

--- 02-Typescript/02-tipos-variable.js
+++ 02-Typescript/02-tipos-variable.js
@@ -5,3 +5,10 @@ segundoNombre = 'Andres';
 cedula = '1233343445'; //Da error read-only
 var URL_POLI = 'http//:www.epn.edu.ec';
 segundoNombre = 123; //Duck Typing
+///
+var nombre;
+var edad = 28;
+var fecha = new Date();
+var casado = false;
+var mascotas = {};
+///
