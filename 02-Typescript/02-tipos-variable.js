d6a3b80fd68f9fa54b27417f9abc0bca8141998e
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-20 17:03:10 -0500

    creacion de una clase y un usuario en typescript

--- 02-Typescript/02-tipos-variable.js
+++ 02-Typescript/02-tipos-variable.js
@@ -12,3 +12,17 @@ var fecha = new Date();
 var casado = false;
 var mascotas = {};
 ///
+//definición de una clase
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, edad, cedula) {
+        this.mNombre = nombre;
+        this.mEdad = edad;
+        this.mCedula = cedula;
+    }
+    Usuario.prototype.imprimirEnConsola = function () {
+        //Template Strings Tilde invertida
+        console.log("El usuario se llama:  " + this.mNombre + " \n        la cedula del usuario es:  " + this.mCedula + "\n        la edad del usuario es: " + this.mEdad);
+    };
+    return Usuario;
+}());
+var usuario = new Usuario('Andres', 22, '1715670830');
--- 02-Typescript/02-tipos-variable.ts
+++ 02-Typescript/02-tipos-variable.ts
@@ -1,5 +1,5 @@
 //inicio
-let segundoNombre:string ='Fernando';
+let segundoNombre: string = 'Fernando';
 const cedula = '1751670830';
 
 segundoNombre = 'Andres';
@@ -7,5 +7,38 @@ cedula = '1233343445';//Da error read-only
 
 
 const URL_POLI = 'http//:www.epn.edu.ec';
-segundoNombre =123;//Duck Typing
+segundoNombre = 123;//Duck Typing
+
+///
+let nombre: String;
+let edad: Number = 28;
+let fecha: Date = new Date();
+let casado: boolean = false;
+let mascotas: object = {};
+///
+
+//definición de una clase
+class Usuario {
+    public mNombre: String;
+    private mEdad: Number;
+    protected mCedula: String;
+    constructor(nombre:String,
+                edad: Number,
+                cedula: String) {
+        this.mNombre=nombre;
+        this.mEdad=edad;
+        this.mCedula=cedula;
+
+    }
+    public imprimirEnConsola(){
+        //Template Strings Tilde invertida
+        console.log(`El usuario se llama:  ${this.mNombre} 
+        la cedula del usuario es:  ${this.mCedula}
+        la edad del usuario es: ${this.mEdad}`)
+    }
+
+}
+
+let usuario: Usuario = new Usuario('Andres',22,'1715670830');
+
 
