e5e72589f36dc6f22155284653907eb45c2942cc
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-20 16:47:16 -0500

    Primeros pasos en Typescript

---
+++ 02-Typescript/02-tipos-variable.js
@@ -0,0 +1,7 @@
+//inicio
+var segundoNombre = 'Fernando';
+var cedula = '1751670830';
+segundoNombre = 'Andres';
+cedula = '1233343445'; //Da error read-only
+var URL_POLI = 'http//:www.epn.edu.ec';
+segundoNombre = 123; //Duck Typing
---
+++ 02-Typescript/02-tipos-variable.ts
@@ -0,0 +1,11 @@
+//inicio
+let segundoNombre:string ='Fernando';
+const cedula = '1751670830';
+
+segundoNombre = 'Andres';
+cedula = '1233343445';//Da error read-only
+
+
+const URL_POLI = 'http//:www.epn.edu.ec';
+segundoNombre =123;//Duck Typing
+
