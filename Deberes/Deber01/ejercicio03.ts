5186d455d3dc5be03ba1838a42c1850dd3d1bb57
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 17:44:50 -0500

    cambios del ejercicio 03
    cambios de presentación y estética

--- Deberes/Deber01/ejercicio03.ts
+++ Deberes/Deber01/ejercicio03.ts
@@ -1,15 +1,16 @@
-function aumentarPoblacionOsos(mesEspecifico){
-    let poblacion=1, poblacionMaxima=10000;
-    for(let mes=1;mes<=mesEspecifico;mes++){
-        poblacion=poblacion*4;
-        if(poblacion<=poblacionMaxima){
-            console.log('Para el mes ',mes,' se prevee que existan ',poblacion, ' osos');
-        }else{
-            console.log('Para el mes ',mes,' la poblacion se excedera con ',poblacion-poblacionMaxima, ' osos');
-            console.log('Removiendo ',poblacion/2,' osos de la poblacion');
-            let nuevaPoblacion=poblacion/2;
+function aumentarPoblacionOsos(mesEspecifico) {
+    let poblacion = 1,
+        poblacionMaxima = 10000;
+    for (let mes = 1; mes <= mesEspecifico; mes++) {
+        poblacion = poblacion * 4;
+        if (poblacion <= poblacionMaxima) {
+            console.log('Para el mes ', mes, ' se prevee que existan ', poblacion, ' osos');
+        } else {
+            console.log('Para el mes ', mes, ' la poblacion se excedera con ', poblacion - poblacionMaxima, ' osos');
+            console.log('Removiendo ', poblacion / 2, ' osos de la poblacion');
+            let nuevaPoblacion = poblacion / 2;
             poblacion = nuevaPoblacion;
-            console.log('Después del mes ',mes, 'la nueva poblacion de osos sera de ',poblacion)
+            console.log('Después del mes ', mes, 'la nueva poblacion de osos sera de ', poblacion)
         }
     }
 }
