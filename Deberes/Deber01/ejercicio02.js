eaa796568dc240cc2380c31d1ea3a0def62ec346
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 12:23:11 -0500

    parte 3 del ejercicio 02
    Creación de una función que calcula el total de MW generados por generadores impares (Principal y Secundario)

--- Deberes/Deber01/ejercicio02.js
+++ Deberes/Deber01/ejercicio02.js
@@ -43,5 +43,34 @@ function medidaDeMegaWattsSoloPares() {
     }
     console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
 }
-medidaDeMegaWattsSoloPares();
+function medidaDeMegaWattsSoloImpares() {
+    var cantidadTotalMW = 0;
+    var cantidadMWGeneradorPrincipal = 62;
+    var cantidadMWGeneradorSecundario = 124;
+    var contadorPrincipal = 1;
+    var contadorSecundario;
+    while (contadorPrincipal <= 4) {
+        if (contadorPrincipal % 2 != 0) {
+            cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
+            console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
+            for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
+                if (contadorSecundario % 2 != 0) {
+                    cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
+                    console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
+                }
+                else {
+                    console.log('Generador Secundario ', contadorSecundario, " esta apagado");
+                }
+            }
+            contadorPrincipal++;
+        }
+        else {
+            console.log('Generador Principal ', contadorPrincipal, " esta apagado");
+            contadorPrincipal++;
+        }
+    }
+    console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
+}
+medidaDeMegaWattsSoloImpares();
+//medidaDeMegaWattsSoloPares()
 //medidaTotalDeMegaWatts()
