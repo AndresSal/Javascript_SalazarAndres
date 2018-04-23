26ad146a9ede59032a14d58e1e4ccee9a20ceb22
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 11:59:48 -0500

    Segunda parte del ejercicio 02
    Creación de la función que emplea solo generadores pares (tanto para generadores Principales como para secundarios)

--- Deberes/Deber01/ejercicio02.ts
+++ Deberes/Deber01/ejercicio02.ts
@@ -17,4 +17,35 @@ function medidaTotalDeMegaWatts() {
     console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
 }
 
-medidaTotalDeMegaWatts()
+function medidaDeMegaWattsSoloPares() {
+    let cantidadTotalMW = 0;
+    let cantidadMWGeneradorPrincipal = 62;
+    let cantidadMWGeneradorSecundario = 124;
+    let contadorPrincipal = 1;
+    let contadorSecundario;
+
+    while (contadorPrincipal <= 4) {
+        if(contadorPrincipal%2==0){
+            cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
+            console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
+            for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
+                if(contadorSecundario%2==0){
+                    cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
+                    console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
+                }else{
+                    console.log('Generador Secundario ',contadorSecundario," esta apagado");
+                }
+            }
+            contadorPrincipal++;
+        }else{
+            console.log('Generador Principal ',contadorPrincipal," esta apagado");
+            contadorPrincipal++;
+        }
+    }
+    console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
+}
+
+medidaDeMegaWattsSoloPares()
+
+//medidaTotalDeMegaWatts()
+
