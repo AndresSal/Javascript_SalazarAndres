fd833fde2a4f8289d6be52695c79c74c7137e0bb
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 12:23:53 -0500

    mejorar la presentación

--- Deberes/Deber01/ejercicio02.ts
+++ Deberes/Deber01/ejercicio02.ts
@@ -25,20 +25,20 @@ function medidaDeMegaWattsSoloPares() {
     let contadorSecundario;
 
     while (contadorPrincipal <= 4) {
-        if(contadorPrincipal%2==0){
+        if (contadorPrincipal % 2 == 0) {
             cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
             console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
             for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
-                if(contadorSecundario%2==0){
+                if (contadorSecundario % 2 == 0) {
                     cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
                     console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
-                }else{
-                    console.log('Generador Secundario ',contadorSecundario," esta apagado");
+                } else {
+                    console.log('Generador Secundario ', contadorSecundario, " esta apagado");
                 }
             }
             contadorPrincipal++;
-        }else{
-            console.log('Generador Principal ',contadorPrincipal," esta apagado");
+        } else {
+            console.log('Generador Principal ', contadorPrincipal, " esta apagado");
             contadorPrincipal++;
         }
     }
@@ -53,20 +53,20 @@ function medidaDeMegaWattsSoloImpares() {
     let contadorSecundario;
 
     while (contadorPrincipal <= 4) {
-        if(contadorPrincipal%2!=0){
+        if (contadorPrincipal % 2 != 0) {
             cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
             console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
             for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
-                if(contadorSecundario%2!=0){
+                if (contadorSecundario % 2 != 0) {
                     cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
                     console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
-                }else{
-                    console.log('Generador Secundario ',contadorSecundario," esta apagado");
+                } else {
+                    console.log('Generador Secundario ', contadorSecundario, " esta apagado");
                 }
             }
             contadorPrincipal++;
-        }else{
-            console.log('Generador Principal ',contadorPrincipal," esta apagado");
+        } else {
+            console.log('Generador Principal ', contadorPrincipal, " esta apagado");
             contadorPrincipal++;
         }
     }
