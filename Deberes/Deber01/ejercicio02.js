791b023207fa553d10e5050d5b1817bb4fd8b7dc
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 11:32:20 -0500

    Inserción del segundo loop, impresión del resultado de cada interación (Principal y Secundaria)

--- Deberes/Deber01/ejercicio02.js
+++ Deberes/Deber01/ejercicio02.js
@@ -3,11 +3,16 @@ function medidaTotalDeMegaWatts() {
     var cantidadMWGeneradorPrincipal = 62;
     var cantidadMWGeneradorSecundario = 124;
     var contadorPrincipal = 1;
-    var contadorSecundario = 0;
+    var contadorSecundario;
     while (contadorPrincipal <= 4) {
         cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
-        console.log('Generador ', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW);
+        console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
+        for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
+            cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
+            console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
+        }
         contadorPrincipal++;
     }
+    console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
 }
 medidaTotalDeMegaWatts();
--- Deberes/Deber01/ejercicio02.ts
+++ Deberes/Deber01/ejercicio02.ts
@@ -3,13 +3,18 @@ function medidaTotalDeMegaWatts() {
     let cantidadMWGeneradorPrincipal = 62;
     let cantidadMWGeneradorSecundario = 124;
     let contadorPrincipal = 1;
-    let contadorSecundario = 0;
+    let contadorSecundario;
 
     while (contadorPrincipal <= 4) {
         cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorPrincipal;
-        console.log('Generador ', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW);
+        console.log('Generador Principal', contadorPrincipal, ' está prendido, añadiendo ', cantidadMWGeneradorPrincipal, 'MW para un total de ', cantidadTotalMW, ' MW');
+        for (contadorSecundario = 1; contadorSecundario <= 15; contadorSecundario++) {
+            cantidadTotalMW = cantidadTotalMW + cantidadMWGeneradorSecundario;
+            console.log('Generador Secundario ', contadorSecundario, ' está prendido, añadiendo ', cantidadMWGeneradorSecundario, 'MW para un total de ', cantidadTotalMW, ' MW');
+        }
         contadorPrincipal++;
     }
+    console.log('La cantidad total de MW generada fue de: ', cantidadTotalMW);
 }
 
 medidaTotalDeMegaWatts()
