2983ac16038594f31117128681f8033bc6b0b01a
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-23 11:02:49 -0500

    mensaje de salida corregido y añadido para arreglos de prueba

--- Deberes/Deber01/ejercicio01.js
+++ Deberes/Deber01/ejercicio01.js
@@ -1,11 +1,13 @@
 function validarNumeroDeStringsEnArreglo(arreglo) {
     var contador = 0;
     for (var aux = 0; aux < arreglo.length; aux++) {
-        console.log(typeof arreglo[aux]);
+        //console.log(typeof arreglo[aux])
         if (typeof arreglo[aux] === "string") {
             contador++;
         }
     }
-    console.log(contador);
+    console.log("el número de strings en el arreglo ", arreglo, "es ", contador);
 }
 validarNumeroDeStringsEnArreglo([5, 'Andres', 3, 'Ana', false, null, '12', undefined]);
+validarNumeroDeStringsEnArreglo([null, 'A', '78', 'Ana', 'false', null, '12', undefined]);
+validarNumeroDeStringsEnArreglo(['5', 'Andres', '3', 'Ana', false, 'null', '12', 'undefined']);
--- Deberes/Deber01/ejercicio01.ts
+++ Deberes/Deber01/ejercicio01.ts
@@ -1,12 +1,14 @@
-function validarNumeroDeStringsEnArreglo(arreglo){
+function validarNumeroDeStringsEnArreglo(arreglo) {
     let contador = 0;
-    for(let aux=0;aux<arreglo.length;aux++){
-        console.log(typeof arreglo[aux])
-        if(typeof arreglo[aux] === "string" ){
+    for (let aux = 0; aux < arreglo.length; aux++) {
+        //console.log(typeof arreglo[aux])
+        if (typeof arreglo[aux] === "string") {
             contador++;
         }
     }
-    console.log(contador);
+    console.log("el número de strings en el arreglo ",arreglo, "es ",contador);
 }
 
-validarNumeroDeStringsEnArreglo([5,'Andres',3,'Ana',false,null,'12',undefined])
+validarNumeroDeStringsEnArreglo([5, 'Andres', 3, 'Ana', false, null, '12', undefined])
+validarNumeroDeStringsEnArreglo([null, 'A','78', 'Ana', 'false', null, '12', undefined])
+validarNumeroDeStringsEnArreglo(['5', 'Andres', '3', 'Ana', false, 'null', '12', 'undefined'])
