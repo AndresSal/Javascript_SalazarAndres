1ac42b66b1cc0e6f65eef4fe9903593c9e69a110
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-24 14:57:23 -0500

    Creación de una fat arrow function

---
+++ 02-Typescript/03-operadores.js
@@ -0,0 +1,6 @@
+//inicio
+var arregloNumeros = [1, 2, 3, 4, 5];
+var sumarDosNumeros = function (numeroUno, numeroDos) {
+    return numeroUno + numeroDos;
+};
+arregloNumeros.forEach();
---
+++ 02-Typescript/03-operadores.ts
@@ -0,0 +1,13 @@
+//inicio
+let arregloNumeros = [1, 2, 3, 4, 5];
+
+
+let sumarDosNumeros = (numeroUno: number, numeroDos: number)=>{//fat arrow function
+return numeroUno + numeroDos;
+};
+
+
+
+arregloNumeros.forEach(
+
+)
