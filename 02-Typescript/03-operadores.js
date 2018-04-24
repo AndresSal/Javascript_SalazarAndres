fd0921636fec97d93ba01c842e3345ef5b51cacf
AndresSal <andres.salazar01@epn.edu.ec>
2018-04-24 15:01:07 -0500

    formas de utilizar la fat arrow function

--- 02-Typescript/03-operadores.js
+++ 02-Typescript/03-operadores.js
@@ -3,4 +3,10 @@ var arregloNumeros = [1, 2, 3, 4, 5];
 var sumarDosNumeros = function (numeroUno, numeroDos) {
     return numeroUno + numeroDos;
 };
+//segunda forma
+var totalEdades = function (arregloDeNumeros) {
+    //codigo extra
+};
+//para retornar un cierto valor
+var facultades = function (a) { return ['Facultad1', 'Facultad2']; }; //sin tener que poner la palabra return
 arregloNumeros.forEach();
--- 02-Typescript/03-operadores.ts
+++ 02-Typescript/03-operadores.ts
@@ -6,7 +6,13 @@ let sumarDosNumeros = (numeroUno: number, numeroDos: number)=>{//fat arrow funct
 return numeroUno + numeroDos;
 };
 
+//segunda forma
+let totalEdades = arregloDeNumeros =>{
+    //codigo extra
+};
 
+//para retornar un cierto valor
+let facultades = a => ['Facultad1', 'Facultad2'];//sin tener que poner la palabra return
 
 arregloNumeros.forEach(
 
